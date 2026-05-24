f1a99d07359cf5b8141dc77905ecbefd7de98c67
Fabian Alberto Ramos <fabianramos@lopezhnos.com.ar>
2026-05-24 11:22:46 -0300

    fix startup cors admin

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://apex-motos-web1-api-sj9n.vercel.app",
+        "https://apex-motos-api-m7p5.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
